fcfe8e2982b06c533844cd4f4867234e7e6ca4ad
asmdhabibullah <asmdhabibullah@yahoo.com>
2022-01-15 22:02:32 +0800

    Updated URI

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ require("dotenv");
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    ENDPOINT: process.env.URI || "http://localhost:4500"
+    ENDPOINT: process.env.URI || "http://localhost:5500"
   },
   future: {
     webpack5: true,
